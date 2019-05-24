7f6e6eb4ed40140a2e4a1b83d08e4066e354ad68
Uros <jeremicuki@gmail.com>
2019-05-25 00:28:27 +0200

    inital commit

---
+++ SS_Emulator/Lexer.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef _LEXER_H_
+#define _LEXER_H_
+
+class Lexer {
+
+public:
+	
+};
+
+#endif // !_LEXER_H_
---
+++ SS_Emulator/Parser.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef _PARSER_H_
+#define _PARSER_H_
+
+
+class Parser {
+
+};
+
+#endif  
+
