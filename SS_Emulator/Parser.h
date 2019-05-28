3e055a1bc7666648af16249ac00f8dd983324b2b
Uros <jeremicuki@gmail.com>
2019-05-28 16:03:36 +0200

    minor commit

--- SS_Emulator/Parser.h
+++ SS_Emulator/Parser.h
@@ -16,7 +16,17 @@ private:
 	Parser();
 public:
 	static Parser* getParser();	// to avoid memmory loss only one parser can be made
+	
+	/*
+		Returns the following
+		nullptr - line contains only whitespace(empty line)
+		ParsedLine(type = 3) - label only
+		ParsedLine(type = 1) - directive
+		ParsedLine(type = 0) - instruction
+		type = 2 is a invalid line and should not be returned, AssemblerException is thrown instead
+	*/
 	ParsedLine* parse(std::queue<std::string>* tokens); // tokens provided from the lexer
+
 	std::string isLabeled(std::string token);	// check if token is a label and returns name, otherwise returns null
 	unsigned char parseType(std::string token); // used on first or second token if labeled to determine Type
 	std::string parseInstructionName(std::string token, unsigned char&  instAttr);	// returns instruction name, to be used only after type parsing; also updates instruction attributes
